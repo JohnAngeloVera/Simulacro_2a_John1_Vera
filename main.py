8005ac1551f2383ff2684a46c2c2be03f071721b
John.V <lhonvera2000@gmail.com>
2026-03-17 12:19:27 +0100

    Creation of the main function

---
+++ main.py
@@ -0,0 +1,10 @@
+def operation_numbers(num1: int, num2: int) -> None:
+
+    add_nums = num1 + num2
+    sub_nums = num1 - num2
+    mul_nums = num1 * num2
+    div_nums = num1 / num2 if num2 != 0 else 0
+    print(f"{add_nums}\n{sub_nums}\n{mul_nums}\n{div_nums}")
+
+
+
